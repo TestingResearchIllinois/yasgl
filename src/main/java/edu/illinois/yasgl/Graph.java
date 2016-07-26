14cb5e4c72b0673bb397abf22f53d6bb740e37bc
Alex Gyori <gyori@illinois.edu>
2016-07-26 11:52:49 -0500

    serialization

--- src/main/java/edu/illinois/yasgl/Graph.java
+++ src/main/java/edu/illinois/yasgl/Graph.java
@@ -3,7 +3,7 @@ package edu.illinois.yasgl;
 import java.util.Collection;
 import java.util.function.Consumer;
 
-public interface Graph<V> {
+public interface Graph<V> extends Serializable {
 
     public Collection<V> getSuccessors(V vertex);
     public Collection<V> getPredecessors(V vertex);
