d919ea7005ca20bcaba98143e003d55ccf9096aa
Alex Gyori <gyori@illinois.edu>
2016-08-01 14:45:48 -0500

    making stuff parallel

---
+++ src/main/java/edu/illinois/yasgl/DFSTask.java
@@ -0,0 +1,43 @@
+package edu.illinois.yasgl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DFSTask<V> implements Runnable {
+
+	private final List<V> vertices;
+	private final Graph<V> graph;
+	private final ConcurrentHashMap<V, Set<V>> resultsMap;
+
+	public DFSTask(List<V> vertices, Graph<V> graph, ConcurrentHashMap<V, Set<V>> results) {
+		this.vertices = vertices;
+		this.graph = graph;
+		this.resultsMap = results;
+	}
+	
+	public void run() {
+		vertices.parallelStream().forEach(vertex -> this.dfs(vertex));
+	}
+	
+	private Set<V> dfs(V vertex) {
+		if (this.resultsMap.putIfAbsent(vertex, Collections.newSetFromMap(new ConcurrentHashMap<V, Boolean>())) != null) {
+			Set<V> result = this.resultsMap.get(vertex);
+			while(!result.contains(vertex)) {
+				try {
+					result.wait(100);
+				} catch (InterruptedException e) { }
+			}
+			return result;
+			
+		}
+		this.graph.getSuccessors(vertex).parallelStream().forEach(v-> this.resultsMap.get(vertex).addAll(dfs(v)));
+
+		this.resultsMap.get(vertex).add(vertex);
+
+		Set<V> result = this.resultsMap.get(vertex);
+		result.notifyAll();
+		return this.resultsMap.get(vertex);
+	}
+}
