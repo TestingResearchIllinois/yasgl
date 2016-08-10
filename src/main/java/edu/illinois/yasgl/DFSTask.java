e9821fd791df25823fa53285de59d4054d62ec2f
Alex Gyori <gyori@illinois.edu>
2016-08-10 17:32:26 -0500

    fixed tc

--- src/main/java/edu/illinois/yasgl/DFSTask.java
+++ src/main/java/edu/illinois/yasgl/DFSTask.java
@@ -1,24 +1,45 @@
 package edu.illinois.yasgl;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DFSTask<V> implements Runnable {
 
-	private final List<V> vertices;
 	private final Graph<V> graph;
-	private final ConcurrentHashMap<V, Set<V>> resultsMap;
+	private final Map<V, Set<V>> resultsMap;
 
-	public DFSTask(List<V> vertices, Graph<V> graph, ConcurrentHashMap<V, Set<V>> results) {
-		this.vertices = vertices;
+	public DFSTask(Graph<V> graph, Map<V, Set<V>> results) {
 		this.graph = graph;
 		this.resultsMap = results;
 	}
 
 	public void run() {
-		vertices.stream().forEach(vertex -> this.dfs(vertex));
+		
+		Queue<V> worklist = new LinkedList<>();
+		worklist.addAll(this.graph.getVertices());
+		
+		graph.getVertices().stream().forEach(v -> this.resultsMap.put(v, new HashSet<>(graph.getSuccessors(v))));
+		graph.getVertices().stream().forEach(v -> this.resultsMap.get(v).add(v));
+		
+		while (!worklist.isEmpty()) {
+			V current = worklist.remove();
+			Set<V> currentTc = this.resultsMap.get(current);
+			
+			Set<V> furtherAway = new HashSet<>();
+			
+			for (V v : currentTc) {
+				furtherAway.addAll(this.resultsMap.get(v));
+			}
+			
+			if (currentTc.addAll(furtherAway)) {
+				worklist.add(current);
+			}
+		}
 	}
 
 	private Set<V> dfs(V vertex) {
--- src/main/java/edu/illinois/yasgl/GraphUtils.java
+++ src/main/java/edu/illinois/yasgl/GraphUtils.java
@@ -10,14 +10,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class GraphUtils {
 	public static <V> Map<V, Set<V>> computeTransitiveClosure(Graph<V> graph) {
 		ConcurrentHashMap<V, Set<V>> forwardReach = new ConcurrentHashMap<>();
-		new DFSTask<V>(new LinkedList<V>(graph.getVertices()), graph, forwardReach).run();
+		new DFSTask<V>(graph, forwardReach).run();
 		
 		return forwardReach;
 	}
 
 	public static <V> Map<V, Set<V>> computeBackwardsTransitiveClosure(Graph<V> graph) {
 		ConcurrentHashMap<V, Set<V>> backwardReach = new ConcurrentHashMap<>();
-		new DFSTask<V>(new LinkedList<V>(graph.getVertices()), graph.inverse(), backwardReach).run();
+		new DFSTask<V>(graph.inverse(), backwardReach).run();
 		
 		return backwardReach;
 	}
